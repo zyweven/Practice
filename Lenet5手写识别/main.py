501d736c94d6a1fbb0cbf80cc687853bc2a2d098
even zhang <33664444+zyweven@users.noreply.github.com>
2021-06-03 23:40:00 +0800

    Add files via upload
    一个基础版本，实现了一个比较丑陋的Lenet识别手写字体。

---
+++ Lenet5手写识别/main.py
@@ -0,0 +1,169 @@
+'''
+lenet 识别手写字体
+'''
+import torch
+from torch import optim
+from torch._C import device
+from torch.autograd.grad_mode import no_grad
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+import os
+from datetime import datetime
+import time
+
+from torch.utils.tensorboard import SummaryWriter
+from model import Lenet5
+
+def train(epoch):
+    net.train()
+    train_loss=0
+    total = 0
+    correct=0
+    for batch_idx,(input,label) in enumerate(train_loadata):
+        if device =='cuda':
+            input,label=input.to(device),label.to(device)
+        optimizer.zero_grad()
+        output=net(input)
+        iter_loss = loss(output,label)
+        iter_loss.backward()
+        optimizer.step()
+        batchsize=label.size(0)
+        # tensorboard 
+        n_iter = batch_idx
+        # print(batch_idx)
+
+        train_loss+=iter_loss.item()
+        _,predicate = output.max(1) #返回每一行中最大的元素并返回索引，返回了两个数组.predicate返回的就是索引 max(0)是返回每列的。
+        
+        total+=label.size(0) 
+
+        # print(label.size(0)) # label.size(0)表示batchsize
+        correct+=predicate.eq(label).sum().item()
+        acc=100*correct/total
+        writer.add_scalar('train/loss',iter_loss,n_iter)
+        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
+            iter_loss.item(),
+            optimizer.param_groups[0]['lr'],
+            epoch=epoch,
+            trained_samples=n_iter,
+            total_samples=int(len(train_loadata.dataset)/batchsize)+(len(train_loadata.dataset)%batchsize ==1)
+        ))
+
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    modelname=type(net).__name__
+    test_loss =0
+    correct =0
+    start=time.time()
+    with torch.no_grad():
+        for batch_idx ,(input,label) in enumerate(test_loadata):
+            if device == 'cuda':
+                input,label = input.to(device),label.to(device)
+            output = net(input)
+            iter_loss = loss(output,label).item()
+            
+            
+            _,predicted = output.max(1)
+            batchsize = predicted.size(0)
+            test_loss +=iter_loss
+            n_iter = batch_idx
+            
+            correct+=predicted.eq(label).sum().item()
+            writer.add_scalar('test/loss',iter_loss,n_iter) 
+            writer.add_scalar('test/acc',correct/((batch_idx+1)*batchsize),)
+        finish = time.time()
+        acc=correct / len(test_loadata.dataset)*100
+        print('Evaluating Network.....')
+        print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+            epoch,
+            test_loss / len(test_loadata.dataset),
+            acc,
+            finish - start
+        ))
+        
+    if acc>best_acc:
+        print('Saving .....')
+        state = {
+            'net':net.state_dict(),
+            'acc':acc,
+            'epcoh':epoch,
+        }
+        if not os.path.isdir(ospath+'/checkpoint'):
+            os.mkdir(ospath+'/checkpoint')
+        torch.save(state, ospath+'/checkpoint/'+modelname+'.pth')
+        best_acc = acc
+
+if __name__=='__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    ospath=os.path.split(os.path.realpath(__file__))[0].replace("\\","/")
+    print('Loading datas .......')
+    transforms_train = transforms.Compose([
+        transforms.RandomCrop(32,padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+    transforms_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    train_data = torchvision.datasets.CIFAR10(
+        root=ospath+'/data', download=True,train=True,transform=transforms_train
+    )
+    test_data = torchvision.datasets.CIFAR10(
+        root=ospath+'./data', download=True,train=False,transform=transforms_test
+    )
+    train_loadata=DataLoader(
+        train_data,batch_size=32,shuffle=True,num_workers=1
+    )
+    test_loadata = DataLoader(
+        test_data,batch_size=32,shuffle=True,num_workers=1
+    )
+    print("data over.......")
+    
+    print('building model.....')
+    net=Lenet5()
+    net = net.to(device)
+
+    print('Prepare for tensorboard......')
+    modelname=type(net).__name__ # get the model name
+    if not os.path.exists(ospath+'/runs'):
+        os.mkdir(ospath+'/runs')
+    DATE_FORMAT = '%A_%d_%B_%Y_%Hh_%Mm_%Ss'
+    #time of we run the script
+    TIME_NOW = datetime.now().strftime(DATE_FORMAT)
+    writer = SummaryWriter(log_dir=os.path.join(
+            ospath+'/runs',modelname,TIME_NOW))
+    # draw the model structure in tensorboard
+    td_inputtensor = torch.Tensor(1,3,32,32)
+    if device == 'cuda' :
+        td_inputtensor=td_inputtensor.cuda()
+    writer.add_graph(net,td_inputtensor)
+    print('tensorboard ready')
+
+    # hyp
+    lr=0.0001
+    TMAX=200
+    momentum=0.9
+    startepoch=0
+    endepoch=200
+    best_acc = 0 
+    print('loss, optimizer and scheduler......')
+    loss = nn.CrossEntropyLoss()
+    # optimizer = optim.adam(net.parameters(),lr)
+    optimizer = optim.SGD(net.parameters(),lr=lr,momentum=momentum)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=TMAX)
+    for epoch in range(startepoch ,endepoch):
+        train(epoch)
+        test(epoch)
+        scheduler.step()
+        
+    writer.close()
---
+++ Lenet5手写识别/model.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as  F
+
+class Lenet5(nn.Module):
+    def __init__(self):
+        super(Lenet5,self).__init__()
+        self.convC1 = nn.Conv2d(3,6,kernel_size=5)
+        self.convC3 = nn.Conv2d(6,16,kernel_size=5)
+        self.FlatC51 = nn.Linear(400,120)
+        self.FlatC52 = nn.Linear(120,84)
+        self.FlatC53 = nn.Linear(84,10)
+        self.Flat = nn.Flatten()
+    
+    def forward(self,x):
+        out=self.convC1(x)
+        out=F.max_pool2d(out,2)
+        out=self.convC3(out)
+        out=F.max_pool2d(out,2)
+        # out = out.view(out.size(0), -1) # 效果貌似和Flatten一样
+        out = self.Flat(out)
+        # print(out.shape)
+        out=self.FlatC51(out)
+        out=self.FlatC52(out)
+        out=self.FlatC53(out)
+        return out
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
+def test(net):
+    # net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+import hiddenlayer as hl
+from os import path
+def draw(name):
+    # name='VGG11'
+    model = eval(name)()
+    # model = VGG('VGG11')
+    ospath=os.path.split(os.path.realpath(__file__))[0].replace("\\","/")
+    print(ospath)
+    savepath=ospath+'/draw'
+    # savepath="D:/Desktop/pytorch-cifar-master/pytorch-cifar-master/draw"
+    savepath=path.join(savepath,name)
+    print(savepath)
+    transforms = [
+        hl.transforms.Fold("Conv > BatchNorm > Relu", "MaxPool"),
+        # Fold Conv, BN, RELU layers into one
+        hl.transforms.Fold("LeakyRelu > MaxPool > Conv > BatchNorm", "ConvBnRelu"),
+        # Fold Conv, BN layers together
+        hl.transforms.Fold("Conv > BatchNorm > LeakyRelu", "ConvBn"),
+        # Fold bottleneck blocks
+        hl.transforms.Fold("""
+            ((ConvBnRelu > ConvBnRelu > ConvBn) | ConvBn) > Add > Relu
+            """, "BottleneckBlock", "Bottleneck Block"),
+        # Fold residual blocks
+        hl.transforms.Fold("""ConvBnRelu > ConvBnRelu > ConvBn > Add > Relu""",
+                        "ResBlock", "Residual Block"),
+        # Fold repeated blocks
+        hl.transforms.FoldDuplicates(),
+    ]
+    im=hl.build_graph(model, torch.zeros([2,3,32,32]),transforms=transforms)
+    im.save(savepath , format="jpg")
+import os
+from torchinfo  import summary
+def summry(net):
+    # params_count(net)
+    summary(net,(2,3,32,32) )
+if __name__ == "__main__":
+    name='Lenet5'
+    net = eval(name)()
+    # test(net)
+    draw(name)
+    summry(net)
+# ==========================================================================================
+# Layer (type:depth-idx)                   Output Shape              Param #
+# ==========================================================================================
+# Lenet5                                   --                        --
+# ├─Conv2d: 1-1                            [2, 6, 28, 28]            456
+# ├─Conv2d: 1-2                            [2, 16, 10, 10]           2,416
+# ├─Flatten: 1-3                           [2, 400]                  --
+# ├─Linear: 1-4                            [2, 120]                  48,120
+# ├─Linear: 1-5                            [2, 84]                   10,164
+# ├─Linear: 1-6                            [2, 10]                   850
+# ==========================================================================================
